8b015e0ff715ce1c5a3e9d3415154209aba2720b
kkerwqss <banykinaks@yandex.ru>
2024-03-21 21:26:38 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,62 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+int countPairs1(int *arr, int len, int value)
+{
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j]) == value) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+int countPairs2(int *arr, int len, int value)
+{
+  int l = 0, r = len - 1;
+  int count = 0;
+  while (arr[r] > value) {
+    r--;
+  }
+  for (int i = 0; i < r; i++) {
+    for (int j = r; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  if (count != 0){
+    return count; 
+  }
   return 0;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int *arr, int len, int value)
+{
+  int count = 0, key = 0;
+  for (int i = 0; i < len; i++) {
+    key = value - arr[i];
+    int l = 0, r = len - 1;
+    while ( l <= r) {
+      int mid = (l + r) / 2;
+      if (arr[mid] == key) {
+        count++;
+        int temp = mid - 1;
+        while (temp >= 0 && arr[temp] == key) {
+          count++;
+          temp--;
+        }
+        temp = mid + 1;
+        while (temp < len && arr[temp] == key) {
+          count++;
+          temp++;
+        }
+        return count;
+      } else if (arr[mid] < key) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+  }
   return 0;
 }
